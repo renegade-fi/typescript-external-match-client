79c8d982fbf18072486c7bcb9a6abfdf24606a2e
Joey Kraut <joey@renegade.fi>
2025-05-09 18:07:07 -0700

    chore: Publish `v0.1.5`

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,7 @@
 /**
  * SDK version information
  * This file is automatically updated during the build process
- * Last updated: 2025-05-08T00:25:05Z
+ * Last updated: 2025-05-10T01:06:48Z
  */
 
-export const VERSION = '0.1.4';
+export const VERSION = '0.1.5';
